4b4b86b49c68c8f7168fb25801ce6bada8aa1177
Patrick Siqueira <118585848+gsPatrick@users.noreply.github.com>
2025-08-28 14:02:22 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+// app/layout.js
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -18,10 +20,34 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   // Este é o layout principal que envolve TODA a aplicação.
-  // Note que ele não tem o atributo `lang` aqui, pois não sabemos o idioma neste nível.
+  // O atributo lang será definido no LangLayout.
   return (
     <html>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P7S6SDR6');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P7S6SDR6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
       </body>
     </html>
